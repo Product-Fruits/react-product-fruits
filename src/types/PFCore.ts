2e2eaaac7c4ac73a9b88d59d6ada96921c71891b
Ladislav Šalom <salom@devhand.com>
2022-09-07 23:15:57 +0200

    type declarations

--- src/types/PFCore.ts
+++ src/types/PFCore.ts
@@ -17,3 +17,22 @@ declare global {
         }
     }
 }
+
+export type ProductFruitsInitOptions = {
+    disableLocationChangeDetection?: boolean;
+    disableBannersAutoMargin?: boolean;
+}
+
+export type ProductFruitsUserObject = {
+    username: string;
+    email?: string;
+    role?: string;
+    signUpAt?: string;
+    firstname?: string;
+    lastname?: string;
+    props?: UserCustomProps
+}
+
+export type UserCustomProps = {
+    [key: string]: string | number | Array<string> | Array<number> | UserCustomProps;
+}
